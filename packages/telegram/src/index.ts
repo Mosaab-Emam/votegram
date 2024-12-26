636515ed7a0b56c4b7a6afea4d55b864dd7e50d5
Mosaab <mosaab.emam123@outlook.com>
2024-12-26 16:28:16 -0500

    feat: install and start with grammy

--- packages/telegram/src/index.ts
+++ packages/telegram/src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello, Telegram!");
+import { config } from "dotenv";
+import { Bot } from "grammy";
+
+config();
+
+// Check for environment variables
+if (!process.env.BOT_TOKEN) {
+  throw new Error("BOT_TOKEN is not set");
+}
+
+const bot = new Bot(process.env.BOT_TOKEN);
+
+bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+bot.on("message", (ctx) => ctx.reply("Got another message!"));
+
+bot.start();
